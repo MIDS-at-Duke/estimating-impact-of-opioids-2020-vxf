07f9a89c2d197acefd9a5e04a38277e2d511988d
satpass <72098721+satpass@users.noreply.github.com>
2020-11-09 20:34:51 -0500

    commit the plot

---
+++ 10_code/Washington.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from plot_functions import diff_in_diff_plot, pre_post_plot
+
+###########################################
+# Plot diff-in-diff and pre-post for Washington
+###########################################
+
+# Washington
+mor_pop = pd.read_csv('../20_intermediate_files/mortality_transformed_final.csv')
+
+# ### Diff in Diff
+df_treatment_WA = mor_pop[mor_pop['State_Abrv'] == 'WA'].copy()
+df_control_WA = mor_pop[(mor_pop['State_Abrv'] == 'UT') | (mor_pop['State_Abrv'] == 'CO') | (mor_pop['State_Abrv'] == 'NC')].copy()
+policy_implementation_year_WA = 2012
+
+plot = diff_in_diff_plot(df_treatment_WA, df_control_WA, policy_implementation_year_WA, 'Deaths_Per_Cap')
+print(plot)
+
+# ### Pre-Post Plots
+plot = pre_post_plot(df_treatment_WA, policy_implementation_year_WA, 'Deaths_Per_Cap')
+print(plot)
