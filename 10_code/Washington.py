e3677f61d1a5d7e5837b164ab48e6679fcbc16d5
satpass <72098721+satpass@users.noreply.github.com>
2020-11-09 20:49:22 -0500

    Changes to the year

--- 10_code/Washington.py
+++ 10_code/Washington.py
@@ -14,6 +14,9 @@ df_treatment_WA = mor_pop[mor_pop['State_Abrv'] == 'WA'].copy()
 df_control_WA = mor_pop[(mor_pop['State_Abrv'] == 'UT') | (mor_pop['State_Abrv'] == 'CO') | (mor_pop['State_Abrv'] == 'NC')].copy()
 policy_implementation_year_WA = 2012
 
+df_treatment_WA = df_treatment_WA[(df_treatment_WA['YEAR'] >= 2007) & (df_treatment_WA['YEAR'] <= 2015)]
+df_control_WA = df_control_WA[(df_control_WA['YEAR'] >= 2007) & (df_control_WA['YEAR'] <= 2015)]
+
 plot = diff_in_diff_plot(df_treatment_WA, df_control_WA, policy_implementation_year_WA, 'Deaths_Per_Cap')
 print(plot)
 
